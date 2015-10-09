0f0952aea29e29b8a8c5fcbadced880e985a4eb0
Pascal COMBES <pascom@orange.fr>
2015-10-09 20:23:53 +0200

    Added more challenging tests for the PlainTextQTestLibParser.
    Currently the parser does not pass the tests.

--- tests/LimitsTest/limitstest.cpp
+++ tests/LimitsTest/limitstest.cpp
@@ -34,7 +34,12 @@ void LimitsTest::testRowNames_data(void)
     QTest::newRow("fun()") << "fun()";
     QTest::newRow("fun(int test)") << "fun(int test)";
     QTest::newRow("str.at().unicode()") << "str.at().unicode()";
+    QTest::newRow("( (a + b) + (c + d) ) + ( (e +f) + (g + h) )") << "( (a + b) + (c + d) ) + ( (e +f) + (g + h) )";
+    QTest::newRow("a)( (a + b) + (c + d) ) + ( (e +f) + (g + h) )") << "a)( (a + b) + (c + d) ) + ( (e +f) + (g + h) )";
     QTest::newRow("( ( (nested) ) )") << "( ( (nested) ) )";
+    QTest::newRow("a)( ( (nested) ) )") << "a)( ( (nested) ) )";
+    QTest::newRow("( ( (double) ) ).( ( (nested) ) )") << "( ( (double) ) ).( ( (nested) ) )";
+    QTest::newRow("a)( ( (double) ) ).( ( (nested) ) )") << "a)( ( (double) ) ).( ( (nested) ) )";
 }
 
 void LimitsTest::testRowNames(void)
