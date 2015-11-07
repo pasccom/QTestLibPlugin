a1d645d554dabe25b77e22d179df1f8dae07bc5c
Pascal COMBES <pascom@orange.fr>
2015-11-07 17:28:04 +0100

    Modified END_SUB_TEST_FUNCTION so that it can be used in if
    without requiring an explicit scope.

--- tests/common/qttestsubfunction.h
+++ tests/common/qttestsubfunction.h
@@ -22,14 +22,19 @@
 #define SUB_TEST_FUNCTION_SUCESS_INDICATOR __mSuccess
 #define HAS_SUB_TEST_FUNCTIONS bool SUB_TEST_FUNCTION_SUCESS_INDICATOR;
 #define BEGIN_SUB_TEST_FUNCTION SUB_TEST_FUNCTION_SUCESS_INDICATOR = false;
-#define END_SUB_TEST_FUNCTION SUB_TEST_FUNCTION_SUCESS_INDICATOR = true;
-#define SUB_TEST_FUNCTION(__fun__) \
-    do { \
-        __fun__; \
-        if (!SUB_TEST_FUNCTION_SUCESS_INDICATOR) \
-            return; \
-        else \
-            SUB_TEST_FUNCTION_SUCESS_INDICATOR = false; \
-    } while(false)
+#define END_SUB_TEST_FUNCTION                                                  \
+    do {                                                                       \
+        SUB_TEST_FUNCTION_SUCESS_INDICATOR = true;                             \
+        return;                                                                \
+    } while(false);                                                            \
+
+#define SUB_TEST_FUNCTION(__fun__)                                             \
+    do {                                                                       \
+        __fun__;                                                               \
+        if (!SUB_TEST_FUNCTION_SUCESS_INDICATOR)                               \
+            return;                                                            \
+        else                                                                   \
+            SUB_TEST_FUNCTION_SUCESS_INDICATOR = false;                        \
+    } while(false)                                                             \
     
 #endif // QT_TEST_SUB_FUNCTION_H
