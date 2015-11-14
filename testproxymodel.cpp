0306878042dc749a7d19d488fd2889a350f6c8b4
Pascal COMBES <pascom@orange.fr>
2015-11-14 19:34:08 +0100

    Implemented the core of the filter.
    Also added a special QToolButton to control it.

--- testproxymodel.cpp
+++ testproxymodel.cpp
@@ -1,7 +1,85 @@
 #include "testproxymodel.h"
 
+#include <QtWidgets>
+#include <QtDebug>
+
 namespace QTestLibPlugin {
 namespace Internal {
+
+TestProxyModel::TestProxyModel(QObject *parent) :
+    QSortFilterProxyModel(parent)
+{
+    mFilters.resize((int) QTestLibModel::LastMessageType);
+
+    for (int i = 0; i < mFilters.size(); i++)
+        mFilters[i] = true;
+}
+
+bool TestProxyModel::filterAcceptsRow(int sourceRow, const QModelIndex& sourceParent) const
+{
+    QModelIndex sourceIndex = sourceModel()->index(sourceRow, 0, sourceParent);
+    Q_ASSERT(sourceIndex.isValid());
+
+    if (!sourceIndex.isValid())
+        return false;
+
+    QVariant messageType = sourceIndex.data(QTestLibModel::ResultRole);
+    Q_ASSERT(messageType.canConvert<QTestLibModel::MessageType>());
+
+    return mFilters.at((int) messageType.value<QTestLibModel::MessageType>());
+}
+
+TestProxyButton::TestProxyButton(QTestLibModel::MessageType messageType, TestProxyModel *proxy, QWidget *parent)
+    :QToolButton(parent), mType(messageType), mProxy(proxy)
+{
+    setIcon(QTestLibModel::messageIcon(messageType));
+    setToolTip(QTestLibModel::resultStringTr(messageType));
+    setCheckable(true);
+    setChecked(true);
+
+    connect(this, SIGNAL(clicked(bool)),
+            this, SLOT(handleClick(bool)));
+}
+
+void TestProxyButton::handleClick(bool checked)
+{
+    if (mProxy == NULL)
+        return;
+
+    if (checked)
+        mProxy->enableMessageType(mType);
+    else
+        mProxy->disableMessageType(mType);
+}
+
+void TestProxyButton::paintEvent(QPaintEvent *pe)
+{
+    QStyleOptionToolButton opt;
+    initStyleOption(&opt);
+
+    QColor color;
+    if (isChecked())
+        color = QColor(0, 255, 0);
+    else
+        color = QColor(255, 0, 0);
+
+    QPainter p(this);
+    if (opt.state & QStyle::State_MouseOver)
+        color.setAlphaF(0.3);
+    else
+        color.setAlphaF(0.15);
+    p.fillRect(pe->rect(), color);
+
+    if (opt.state & QStyle::State_HasFocus)
+        color.setAlphaF(0.3);
+    else
+        color.setAlphaF(0.);
+    p.setBrush(Qt::NoBrush);
+    p.setPen(color);
+    p.drawRect(pe->rect().adjusted(0, 0, -1, -1));
+
+    style()->drawControl(QStyle::CE_ToolButtonLabel, &opt, &p);
+}
 
 } // namespace Internal
 } // namespace QTestLibPlugin
--- testproxymodel.h
+++ testproxymodel.h
@@ -1,16 +1,41 @@
 #ifndef TESTPROXYMODEL_H
 #define TESTPROXYMODEL_H
 
+#include "qtestlibmodel.h"
+
 #include <QSortFilterProxyModel>
+#include <QToolButton>
 
 namespace QTestLibPlugin {
 namespace Internal {
 
 class TestProxyModel : public QSortFilterProxyModel
 {
+    Q_OBJECT
 public:
-    inline TestProxyModel(QObject *parent = NULL) :
-        QSortFilterProxyModel(parent) {}
+    TestProxyModel(QObject *parent = NULL);
+    inline void enableMessageType(QTestLibModel::MessageType type) {mFilters[(int) type] = true; invalidateFilter();}
+    inline void disableMessageType(QTestLibModel::MessageType type) {mFilters[(int) type] = false; invalidateFilter();}
+    inline void toogleMessageType(QTestLibModel::MessageType type) {mFilters[(int) type] = !mFilters.at((int) type); invalidateFilter();}
+    inline bool isMessageTypeEnabled(QTestLibModel::MessageType type) const {return mFilters.at((int) type);}
+protected:
+    bool filterAcceptsRow(int sourceRow, const QModelIndex& sourceParent) const;
+private:
+    QVector<bool> mFilters;
+};
+
+class TestProxyButton : public QToolButton
+{
+    Q_OBJECT
+public:
+    TestProxyButton(QTestLibModel::MessageType messageType, TestProxyModel *proxy = NULL, QWidget *parent = NULL);
+protected:
+    void paintEvent(QPaintEvent *pe);
+private slots:
+    void handleClick(bool checked);
+private:
+    QTestLibModel::MessageType mType;
+    TestProxyModel *mProxy;
 };
 
 } // namespace Internal
