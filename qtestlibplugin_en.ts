89ed8a9d3eeb814023903b8ff4bf7fb183385fc0
Pascal COMBES <pascom@orange.fr>
2015-11-21 17:41:12 +0100

    Added slot translation.

--- qtestlibplugin_en.ts
+++ qtestlibplugin_en.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>QTestLibPlugin::Internal::QTestLibArgsParser</name>
     <message>
@@ -32,142 +32,147 @@
 <context>
     <name>QTestLibPlugin::Internal::QTestLibModel</name>
     <message>
-        <location filename="qtestlibmodel.cpp" line="342"/>
+        <location filename="qtestlibmodel.cpp" line="347"/>
         <source>Test skipped</source>
         <translation>Test skipped</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="344"/>
+        <location filename="qtestlibmodel.cpp" line="349"/>
         <source>Test passed</source>
         <translation>Test passed</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="346"/>
+        <location filename="qtestlibmodel.cpp" line="351"/>
         <source>Test passed unexpectedly</source>
         <translation>Test passed unexpectedly</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="348"/>
+        <location filename="qtestlibmodel.cpp" line="353"/>
         <source>Blacklisted test passed</source>
         <translation>Blacklisted test passed</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="350"/>
+        <location filename="qtestlibmodel.cpp" line="355"/>
         <source>Test failed</source>
         <translation>Test failed</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="352"/>
+        <location filename="qtestlibmodel.cpp" line="357"/>
         <source>Test failed unexpectedly</source>
         <translation>Test failed unexpectedly</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="354"/>
+        <location filename="qtestlibmodel.cpp" line="359"/>
         <source>Blacklisted test failed</source>
         <translation>Blacklisted test failed</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="383"/>
+        <location filename="qtestlibmodel.cpp" line="396"/>
+        <source>Slot</source>
+        <translation>Slot</translation>
+    </message>
+    <message>
+        <location filename="qtestlibmodel.cpp" line="397"/>
         <source>Signal</source>
         <oldsource>Signal  </oldsource>
         <translation>Signal</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="384"/>
+        <location filename="qtestlibmodel.cpp" line="398"/>
         <source>Duration</source>
         <translation>Duration</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="385"/>
+        <location filename="qtestlibmodel.cpp" line="399"/>
         <source>Result</source>
         <oldsource>Result  </oldsource>
         <translation>Result</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="386"/>
+        <location filename="qtestlibmodel.cpp" line="400"/>
         <source>QDebug</source>
         <oldsource>QDebug  </oldsource>
         <translation>QDebug message</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="387"/>
+        <location filename="qtestlibmodel.cpp" line="401"/>
         <source>Info</source>
         <oldsource>Info    </oldsource>
         <translation>Info message</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="388"/>
+        <location filename="qtestlibmodel.cpp" line="402"/>
         <source>Warning</source>
         <oldsource>Warning </oldsource>
         <translation>Warning message</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="389"/>
+        <location filename="qtestlibmodel.cpp" line="403"/>
         <source>QWarning</source>
         <translation>QWarning message</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="390"/>
+        <location filename="qtestlibmodel.cpp" line="404"/>
         <source>QSystem</source>
         <oldsource>QSystem </oldsource>
         <translation>QSystem message</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="391"/>
+        <location filename="qtestlibmodel.cpp" line="405"/>
         <source>QFatal</source>
         <oldsource>QFatal  </oldsource>
         <translation>QFatal message</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="392"/>
+        <location filename="qtestlibmodel.cpp" line="406"/>
         <source>Unknown</source>
         <oldsource>Unknown </oldsource>
         <translation>Unknown item</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="393"/>
+        <location filename="qtestlibmodel.cpp" line="407"/>
         <source>Skip</source>
         <oldsource>Skip    </oldsource>
         <translatorcomment>Put in preterite form</translatorcomment>
         <translation>Skiped test</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="394"/>
+        <location filename="qtestlibmodel.cpp" line="408"/>
         <source>Pass</source>
         <oldsource>Pass    </oldsource>
         <translatorcomment>Put in preterite form</translatorcomment>
         <translation>Passed test</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="395"/>
+        <location filename="qtestlibmodel.cpp" line="409"/>
         <source>BPass</source>
         <oldsource>BPass   </oldsource>
         <translatorcomment>Put in preterite form</translatorcomment>
         <translation>Blaclisted passed test</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="396"/>
+        <location filename="qtestlibmodel.cpp" line="410"/>
         <source>XPass</source>
         <oldsource>XPass   </oldsource>
         <translatorcomment>Put in preterite form</translatorcomment>
         <translation>Unexpectedly passed test</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="397"/>
+        <location filename="qtestlibmodel.cpp" line="411"/>
         <source>XFail</source>
         <oldsource>XFail   </oldsource>
         <translatorcomment>Put in preterite form</translatorcomment>
         <translation>Expectedly failed test</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="398"/>
+        <location filename="qtestlibmodel.cpp" line="412"/>
         <source>BFail</source>
         <oldsource>BFail   </oldsource>
         <translatorcomment>Put in preterite form</translatorcomment>
         <translation>Blacklisted failed test</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="399"/>
+        <location filename="qtestlibmodel.cpp" line="413"/>
         <source>Fail</source>
         <oldsource>Fail    </oldsource>
         <translatorcomment>Put in preterite form</translatorcomment>
@@ -177,7 +182,7 @@
 <context>
     <name>QTestLibPlugin::Internal::TestOutputPane</name>
     <message>
-        <location filename="testoutputpane.h" line="48"/>
+        <location filename="testoutputpane.h" line="50"/>
         <source>Test output</source>
         <translation>Test output</translation>
     </message>
--- qtestlibplugin_fr.ts
+++ qtestlibplugin_fr.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="fr_FR">
+<TS version="2.1" language="fr_FR">
 <context>
     <name>QTestLibPlugin::Internal::QTestLibArgsParser</name>
     <message>
@@ -32,136 +32,141 @@
 <context>
     <name>QTestLibPlugin::Internal::QTestLibModel</name>
     <message>
-        <location filename="qtestlibmodel.cpp" line="342"/>
+        <location filename="qtestlibmodel.cpp" line="347"/>
         <source>Test skipped</source>
         <translation>Test sauté</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="344"/>
+        <location filename="qtestlibmodel.cpp" line="349"/>
         <source>Test passed</source>
         <translation>Réussite du test</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="346"/>
+        <location filename="qtestlibmodel.cpp" line="351"/>
         <source>Test passed unexpectedly</source>
         <translation>Réussite inattendue du test</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="348"/>
+        <location filename="qtestlibmodel.cpp" line="353"/>
         <source>Blacklisted test passed</source>
         <translation>Réussite du test sur liste noire</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="350"/>
+        <location filename="qtestlibmodel.cpp" line="355"/>
         <source>Test failed</source>
         <translation>Échec du test</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="352"/>
+        <location filename="qtestlibmodel.cpp" line="357"/>
         <source>Test failed unexpectedly</source>
         <translation>Échec attendu du test</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="354"/>
+        <location filename="qtestlibmodel.cpp" line="359"/>
         <source>Blacklisted test failed</source>
         <translation>Échec d&apos;un test sur liste noire</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="383"/>
+        <location filename="qtestlibmodel.cpp" line="396"/>
+        <source>Slot</source>
+        <translation>Slot</translation>
+    </message>
+    <message>
+        <location filename="qtestlibmodel.cpp" line="397"/>
         <source>Signal</source>
         <oldsource>Signal  </oldsource>
         <translation>Signal</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="384"/>
+        <location filename="qtestlibmodel.cpp" line="398"/>
         <source>Duration</source>
         <translation>Durée</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="385"/>
+        <location filename="qtestlibmodel.cpp" line="399"/>
         <source>Result</source>
         <oldsource>Result  </oldsource>
         <translation>Résultat</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="386"/>
+        <location filename="qtestlibmodel.cpp" line="400"/>
         <source>QDebug</source>
         <oldsource>QDebug  </oldsource>
         <translation>Message QDebug</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="387"/>
+        <location filename="qtestlibmodel.cpp" line="401"/>
         <source>Info</source>
         <oldsource>Info    </oldsource>
         <translation>Message d&apos;info</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="388"/>
+        <location filename="qtestlibmodel.cpp" line="402"/>
         <source>Warning</source>
         <oldsource>Warning </oldsource>
         <translation>Message important</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="389"/>
+        <location filename="qtestlibmodel.cpp" line="403"/>
         <source>QWarning</source>
         <translation>Message QWarning</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="390"/>
+        <location filename="qtestlibmodel.cpp" line="404"/>
         <source>QSystem</source>
         <oldsource>QSystem </oldsource>
         <translation>Message QSystem</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="391"/>
+        <location filename="qtestlibmodel.cpp" line="405"/>
         <source>QFatal</source>
         <oldsource>QFatal  </oldsource>
         <translation>Message QFatal</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="392"/>
+        <location filename="qtestlibmodel.cpp" line="406"/>
         <source>Unknown</source>
         <oldsource>Unknown </oldsource>
         <translation>Item inconnu</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="393"/>
+        <location filename="qtestlibmodel.cpp" line="407"/>
         <source>Skip</source>
         <oldsource>Skip    </oldsource>
         <translation>Test sauté</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="394"/>
+        <location filename="qtestlibmodel.cpp" line="408"/>
         <source>Pass</source>
         <oldsource>Pass    </oldsource>
         <translation>Test réussi</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="395"/>
+        <location filename="qtestlibmodel.cpp" line="409"/>
         <source>BPass</source>
         <oldsource>BPass   </oldsource>
         <translation>Test sur liste noire réussi</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="396"/>
+        <location filename="qtestlibmodel.cpp" line="410"/>
         <source>XPass</source>
         <oldsource>XPass   </oldsource>
         <translation>Test réussi (inattendu)</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="397"/>
+        <location filename="qtestlibmodel.cpp" line="411"/>
         <source>XFail</source>
         <oldsource>XFail   </oldsource>
         <translation>Test échoué (attendu)</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="398"/>
+        <location filename="qtestlibmodel.cpp" line="412"/>
         <source>BFail</source>
         <oldsource>BFail   </oldsource>
         <translation>Test sur liste noire échoué</translation>
     </message>
     <message>
-        <location filename="qtestlibmodel.cpp" line="399"/>
+        <location filename="qtestlibmodel.cpp" line="413"/>
         <source>Fail</source>
         <oldsource>Fail    </oldsource>
         <translation>Test échoué</translation>
@@ -170,7 +175,7 @@
 <context>
     <name>QTestLibPlugin::Internal::TestOutputPane</name>
     <message>
-        <location filename="testoutputpane.h" line="48"/>
+        <location filename="testoutputpane.h" line="50"/>
         <source>Test output</source>
         <translation>Sortie de test</translation>
     </message>
