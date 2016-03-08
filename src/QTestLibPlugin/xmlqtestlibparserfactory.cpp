dc401535f1c12a6944ce45c2b74a0d37506aa379
Pascal COMBES <pascom@orange.fr>
2016-03-08 21:07:07 +0100

    Used the generic heuristic for xmlqtestlibparserfactory.

--- src/QTestLibPlugin/xmlqtestlibparserfactory.cpp
+++ src/QTestLibPlugin/xmlqtestlibparserfactory.cpp
@@ -18,14 +18,7 @@
 
 #include "xmlqtestlibparserfactory.h"
 
-#include "qtestlibargsparser.h"
-#include "testrunconfiguration.h"
-
 #include <projectexplorer/runconfiguration.h>
-#include <projectexplorer/localapplicationrunconfiguration.h>
-#include <projectexplorer/target.h>
-#include <qmakeprojectmanager/qmakeproject.h>
-#include <qmakeprojectmanager/qmakenodes.h>
 
 #include <QtDebug>
 
@@ -40,52 +33,6 @@ AbstractTestParser* XMLQTestLibParserFactory::getParserInstance(ProjectExplorer:
         return NULL;
     qDebug() << "XMLTextQTestLibParser can parse this file";
     return new XMLQTestLibParser(runConfiguration);
-}
-
-bool XMLQTestLibParserFactory::canParseRunConfiguration(ProjectExplorer::RunConfiguration* runConfiguration) const
-{
-    Q_ASSERT(runConfiguration != NULL);
-
-    TestRunConfiguration* testRunConfiguration = qobject_cast<TestRunConfiguration*>(runConfiguration);
-    if (testRunConfiguration == NULL)
-        return false;
-
-    QRegExp extraTestArgsRegExp(QLatin1String("TESTARGS=\"[^\"]*\""));
-    if (extraTestArgsRegExp.indexIn(testRunConfiguration->commandLineArguments()) == -1)
-        return true;
-    return canParseArguments(runConfiguration);
-}
-
-bool XMLQTestLibParserFactory::canParseModule(ProjectExplorer::RunConfiguration *runConfiguration) const
-{
-    Q_ASSERT(runConfiguration != NULL);
-
-    QmakeProjectManager::QmakeProject *qMakeProject = qobject_cast<QmakeProjectManager::QmakeProject *>(runConfiguration->target()->project());
-    if (qMakeProject != NULL) {
-        foreach(QmakeProjectManager::QmakeProFileNode *pro, qMakeProject->allProFiles()) {
-            qDebug() << "Project name:" << pro->displayName();
-            qDebug() << "QT variable:" << pro->variableValue(QmakeProjectManager::QtVar);
-            if (pro->variableValue(QmakeProjectManager::QtVar).contains(QLatin1String("testlib"), Qt::CaseSensitive))
-                return canParseArguments(runConfiguration);
-        }
-    }
-
-    return false;
-}
-
-bool XMLQTestLibParserFactory::canParseArguments(ProjectExplorer::RunConfiguration *runConfiguration) const
-{
-    ProjectExplorer::LocalApplicationRunConfiguration *localRunConfig = qobject_cast<ProjectExplorer::LocalApplicationRunConfiguration *>(runConfiguration);
-    if (localRunConfig != NULL) {
-        qDebug() << "Command line args:" << localRunConfig->commandLineArguments();
-        QTestLibArgsParser parser(localRunConfig->commandLineArguments());
-
-        return ((parser.error() == QTestLibArgsParser::NoError)
-             && (parser.outputFormat() == QTestLibArgsParser::XmlFormat)
-              && parser.outFileName().toString().isEmpty());
-    }
-
-    return false;
 }
 
 } // namespace Internal
